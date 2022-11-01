00e200889ef6c4d107366b4cf60e4c121b1ff9e4
CharlesLe114 <baotoanle114@gmail.com>
2022-11-01 19:11:16 +0000

    add bootstrap server

--- modules/location-grpc-producer/consumer.py
+++ modules/location-grpc-producer/consumer.py
@@ -3,6 +3,6 @@ from kafka import KafkaConsumer
 
 TOPIC_NAME = 'location_data'
 
-consumer = KafkaConsumer(TOPIC_NAME)
+consumer = KafkaConsumer(TOPIC_NAME,bootstrap_servers=['localhost:9092'])
 for message in consumer:
     print (message)
