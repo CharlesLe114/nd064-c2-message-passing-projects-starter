d7d1a56eee96e2381b9c3be7b9b13c2cc82cd057
CharlesLe114 <baotoanle114@gmail.com>
2022-11-01 04:45:41 +0000

    add connection api

--- modules/person-api/app/udaconnect/services.py
+++ modules/person-api/app/udaconnect/services.py
@@ -6,7 +6,7 @@ from app.udaconnect.models import Person
 from app.udaconnect.schemas import PersonSchema
 
 logging.basicConfig(level=logging.WARNING)
-logger = logging.getLogger("udaconnect-api")
+logger = logging.getLogger("udaconnect-person-api")
 
 class PersonService:
     @staticmethod
