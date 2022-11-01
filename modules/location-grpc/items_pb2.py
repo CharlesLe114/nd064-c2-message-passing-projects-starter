08803c453e83495471c3ad26de34b54e3c4bc436
toanlebao <toan.lebao@halliburton.com>
2022-11-01 16:03:36 +0700

    tets

--- modules/location-grpc/items_pb2.py
+++ modules/location-grpc/items_pb2.py
@@ -13,7 +13,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0bitems.proto\"l\n\x0fLocationMessage\x12\n\n\x02id\x18\x01 \x01(\x05\x12\x11\n\tperson_id\x18\x02 \x01(\x05\x12\x11\n\tlongitude\x18\x03 \x01(\t\x12\x10\n\x08latitude\x18\x04 \x01(\t\x12\x15\n\rcreation_time\x18\x05 \x01(\x03\"P\n\x10LocationResponse\x12\x11\n\tperson_id\x18\x01 \x01(\x05\x12\x15\n\rcreation_time\x18\x02 \x01(\x03\x12\x12\n\ncoordinate\x18\x03 \x01(\t2B\n\x11\x43onnectionService\x12-\n\x06\x43reate\x12\x10.LocationMessage\x1a\x11.LocationResponseb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0bitems.proto\"l\n\x0fLocationMessage\x12\n\n\x02id\x18\x01 \x01(\x05\x12\x11\n\tperson_id\x18\x02 \x01(\x05\x12\x11\n\tlongitude\x18\x03 \x01(\t\x12\x10\n\x08latitude\x18\x04 \x01(\t\x12\x15\n\rcreation_time\x18\x05 \x01(\x03\",\n\x05Point\x12\x10\n\x08latitude\x18\x01 \x01(\x05\x12\x11\n\tlongitude\x18\x02 \x01(\x05\"X\n\x10LocationResponse\x12\x11\n\tperson_id\x18\x01 \x01(\x05\x12\x15\n\rcreation_time\x18\x02 \x01(\x03\x12\x1a\n\ncoordinate\x18\x03 \x01(\x0b\x32\x06.Point2B\n\x11\x43onnectionService\x12-\n\x06\x43reate\x12\x10.LocationMessage\x1a\x11.LocationResponseb\x06proto3')
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'items_pb2', globals())
@@ -22,8 +22,10 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   DESCRIPTOR._options = None
   _LOCATIONMESSAGE._serialized_start=15
   _LOCATIONMESSAGE._serialized_end=123
-  _LOCATIONRESPONSE._serialized_start=125
-  _LOCATIONRESPONSE._serialized_end=205
-  _CONNECTIONSERVICE._serialized_start=207
-  _CONNECTIONSERVICE._serialized_end=273
+  _POINT._serialized_start=125
+  _POINT._serialized_end=169
+  _LOCATIONRESPONSE._serialized_start=171
+  _LOCATIONRESPONSE._serialized_end=259
+  _CONNECTIONSERVICE._serialized_start=261
+  _CONNECTIONSERVICE._serialized_end=327
 # @@protoc_insertion_point(module_scope)
