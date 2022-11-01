28c2634c2ddefe610f927073ab8c972eb1e4355a
CharlesLe114 <baotoanle114@gmail.com>
2022-11-01 09:34:36 +0000

    app check

---
+++ modules/location-grpc/writer.py
@@ -0,0 +1,24 @@
+import grpc
+import items_pb2
+import items_pb2_grpc
+
+"""
+Sample implementation of a writer that can be used to write messages to gRPC.
+"""
+
+print("Sending sample payload...")
+
+channel = grpc.insecure_channel("localhost:5005")
+stub = items_pb2_grpc.LocationServiceStub(channel)
+
+# Update this with desired payload
+location = items_pb2.LocationMessage(
+    id="1",
+    person_id="2",
+    longitude="test_lon",
+    latitude="test_la",
+    creation_time=1667295256
+)
+
+
+response = stub.Create(location)
