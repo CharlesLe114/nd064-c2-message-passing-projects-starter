674050f336bd7da1fee1b43a1cf8e383c201d824
CharlesLe114 <baotoanle114@gmail.com>
2022-11-01 09:44:22 +0000

    syntax

--- modules/location-grpc/main.py
+++ modules/location-grpc/main.py
@@ -19,9 +19,9 @@ class LocationServicer(items_pb2_grpc.LocationServiceServicer):
         }
         point = items_pb2.Point(latitude=131934, longitude=414837)
         return_value = {
-            "person_id" = request.id,
-            "creation_time" = request.creation_time
-            "coordinate" = point
+            "person_id": request.id,
+            "creation_time": request.creation_time,
+            "coordinate": point
         }
         return return_value
 
