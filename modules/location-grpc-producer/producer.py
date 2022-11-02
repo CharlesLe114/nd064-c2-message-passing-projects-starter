e796829785d97df3b9f87611c24e6159a24fb9f5
CharlesLe114 <baotoanle114@gmail.com>
2022-11-02 04:45:41 +0000

    test produce

---
+++ modules/location-grpc-producer/producer.py
@@ -0,0 +1,19 @@
+from kafka import KafkaProducer
+
+
+TOPIC_NAME = 'items'
+KAFKA_SERVER = 'localhost:9092'
+
+producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
+
+request_value = {
+    "person_id": 2,
+    "longitude": 1.23,
+    "latitude": 3.43,
+    "creation_time": 1667295256
+}
+
+data = json.dumps(request_value)
+producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
+producer.send(TOPIC_NAME, value=data)
+producer.flush()
