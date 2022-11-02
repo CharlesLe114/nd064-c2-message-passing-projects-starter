372ae03ef0c6b8b25faf6e2d087ed1c1cebfaeff
CharlesLe114 <baotoanle114@gmail.com>
2022-11-02 05:11:17 +0000

    change port

--- modules/location-grpc-producer/writer.py
+++ modules/location-grpc-producer/writer.py
@@ -8,7 +8,7 @@ Sample implementation of a writer that can be used to write messages to gRPC.
 
 print("Sending sample payload...")
 
-channel = grpc.insecure_channel("localhost:5005")
+channel = grpc.insecure_channel("localhost:30005")
 stub = locations_pb2_grpc.LocationServiceStub(channel)
 
 # Update this with desired payload
