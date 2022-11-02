e71ffd8e8b044898beb0c7267bfc40456987d957
CharlesLe114 <baotoanle114@gmail.com>
2022-11-02 04:56:10 +0000

    add api producer

---
+++ modules/location-grpc-api-producer/main.py
@@ -0,0 +1,59 @@
+import time
+from concurrent import futures
+
+import grpc
+import locations_pb2
+import locations_pb2_grpc
+import json
+
+from kafka import KafkaProducer
+from datetime import datetime, timezone
+
+TOPIC_NAME = 'location_data'
+KAFKA_SERVER = "localhost:9092"
+
+def response_test(request_value):
+    res = locations_pb2.LocationResponse(
+        person_id = request_value["person_id"],
+        creation_time = request_value["creation_time"],
+        coordinate = locations_pb2.Point(
+            latitude = request_value["latitude"],
+            longitude = request_value["longitude"]
+        )
+    )
+    return res
+
+class LocationServicer(locations_pb2_grpc.LocationServiceServicer):
+    def Create(self, request, context):
+        print("Received a message!")
+
+        request_value = {
+            "person_id": request.person_id,
+            "longitude": request.longitude,
+            "latitude": request.latitude,
+            "creation_time": request.creation_time
+        }
+
+        data = json.dumps(request_value).encode('utf-8') # convert dict to string -> match with kafka requirement
+        producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
+        producer.send(TOPIC_NAME, value=data)
+        producer.flush()
+
+        print(data)
+        return locations_pb2.LocationMessage(**request_value)
+
+
+# Initialize gRPC server
+server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
+locations_pb2_grpc.add_LocationServiceServicer_to_server(LocationServicer(), server)
+
+
+print("Server starting on port 5005...")
+server.add_insecure_port("[::]:5005")
+server.start()
+# Keep thread alive
+try:
+    while True:
+        time.sleep(86400)
+except KeyboardInterrupt:
+    server.stop(0)
---
+++ modules/location-grpc-api-producer/writer.py
@@ -0,0 +1,25 @@
+import grpc
+import locations_pb2
+import locations_pb2_grpc
+
+"""
+Sample implementation of a writer that can be used to write messages to gRPC.
+"""
+
+print("Sending sample payload...")
+
+channel = grpc.insecure_channel("localhost:5005")
+stub = locations_pb2_grpc.LocationServiceStub(channel)
+
+# Update this with desired payload
+location = locations_pb2.LocationMessage(
+    person_id=2,
+    longitude=1.23,
+    latitude=3.43,
+    creation_time=1667295256
+)
+
+
+response = stub.Create(location)
+
+print(response)
